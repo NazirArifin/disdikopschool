4b2a571433ae7dd9d5380f8d35c8691fa52fcf21
NazirArifin <ceylon.rizan@gmail.com>
2021-08-05 00:17:20 +0700

    vueconfig

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: {
+    target: "electron-renderer",
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale\/(en|id)\.js$/,
+        contextRegExp: /moment$/
+      }),
+      new MomentLocalesPlugin({
+        localesToKeep: ['id']
+      })
+    ],
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        publish: [{
+          provider: 'github',
+          private: true,
+          releaseType: 'release'
+        }],
+        win: {
+          icon: './icon.ico'
+        }
+      }
+    }
+  }
+}
